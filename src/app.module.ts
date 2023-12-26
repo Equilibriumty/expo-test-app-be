017a99cd9d1e134333453a33e2816f5723f34b9e
Equilibriumty <fanflackplay@gmail.com>
2023-12-26 13:43:28 +0200

    feat: implement real e2e tests

--- src/app.module.ts
+++ src/app.module.ts
@@ -14,8 +14,14 @@ import { ProfileModule } from './profile/profile.module';
         host: configService.get('DB_HOST'),
         port: configService.get('DB_PORT'),
         username: configService.get('DB_USERNAME'),
-        password: configService.get('DB_PASSWORD'),
-        database: configService.get('DB_NAME'),
+        password:
+          process.env.NODE_ENV === 'test'
+            ? configService.get('TEST_DB_PASSWORD')
+            : configService.get('DB_PASSWORD'),
+        database:
+          process.env.NODE_ENV === 'test'
+            ? configService.get('TEST_DB_NAME')
+            : configService.get('DB_NAME'),
         synchronize: true,
         entities: [__dirname + '/**/*.entity{.ts,.js}'],
       }),
